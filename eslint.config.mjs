eeeeed97d3f2fe53f7999a4bcc09aee6355a7028
Carl Lee <sjlee87@kakao.com>
2025-08-12 18:02:07 +0900

    타입 재수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    plugins: {
+      "@typescript-eslint": (await import("@typescript-eslint/eslint-plugin")).default,
+    },
     rules: {
       "@typescript-eslint/no-explicit-any": "error",
       "@typescript-eslint/no-unused-vars": "error",
